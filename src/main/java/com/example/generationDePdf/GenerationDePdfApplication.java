21bbd44bdb6748692634b1986347ce504525358c
doczoub <zoubeyamadou1@gmail.com>
2024-08-19 16:56:31 +0000

    first commit

---
+++ src/main/java/com/example/generationDePdf/GenerationDePdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.generationDePdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationDePdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationDePdfApplication.class, args);
+	}
+
+}
